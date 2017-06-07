265ef6735a094efecd5ed7c573f66f572638b1e4
kobenein <kobenein@gmail.com>
2017-06-07 19:28:58 +0800

    label maker

---
+++ Labels/新文字文件.bat
@@ -0,0 +1,12 @@
+REM convert -list font >> font.txt
+
+
+
+
+
+
+REM convert -channel RGBA -font Noto-Sans-Mono-CJK-TC-Bold -pointsize 40  "label:°ê1 49K" -transparent white label.PNG
+
+convert -font Noto-Sans-Mono-CJK-TC-Bold -pointsize 40  "label:°ê1 49K" -transparent white label~.PNG
+
+pause
